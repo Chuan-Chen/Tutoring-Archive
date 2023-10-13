b589e44ae0814996ef588fa00d23e7d98ed16c7f
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2023-10-13 14:50:16 -0400

    update

--- test.cpp
+++ test.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 
 int main() {
+
+    char s;
+
+    cout << "input char: ";
+
+    cin >> s; 
+
+
+    switch(s){
+        case 'c': 
+            cout << "hi" << endl;
+            break;
+        case 'b': 
+            cout << "bye" << endl;
+            break;
+        
+
+    }
     
 
 
