c904550567b23ba237a0a9828ecbc90b1a394c94
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2023-12-01 12:56:41 -0500

    tut

--- test.cpp
+++ test.cpp
@@ -1,41 +1,47 @@
-#include <iostream>
-using namespace std;
+void merge(int *a, int start, int middle, int end);
 
-struct Inventory{
-    string item = "Default sword";
-};
-
-struct Player{
-    private:
-    string Name = "NA";
-    Inventory inventory1;
-
-    public:
-    Player(string n): Name(n){};
-    Inventory getInventory(){
-        return inventory1;
+//sort function is breaking up the array into halves
+void sort(int *a, int start, int end){
+    if(start < end){
+        int middle = (start + end)/2;
+        sort(a, start, middle); //90, 70, 20, 10
+        sort(a, middle + 1, end); //40, 50, 20, 100
+        merge(a, start, middle, end);
     }
-};
-
-struct Game{
-    Player p1{playerName};
-    string playerName;  
-
-    Game(string n): playerName(n){};
-    Game(): playerName("NA"){};
-    Player getPlayer(){
-        return p1;
-    }
-
-};
-
-
-int main(){
-
-    Game GTA{};
-    
-    cout << GTA.getPlayer().getInventory().item << endl;
-    cout << GTA.playerName;
-
-    return 0;
 }
+
+/*
+Loop 1
+    [...] 0    7 
+sort(a, start, size - 1)
+     0   1   2   3   4   5   6   7
+a = [90, 70, 20, 10, 40, 50, 20, 100] 
+start = 0, middle = (start + end)/2 = 3, end = 7
+            [...]   0      3
+    line 62 sort(a, start, middle)
+                [...]    0      3
+            sort(int *a, start, end)
+        Recursion 1
+            a = [90, 70, 20, 10, 40, 50, 20, 100]
+            start = 0, middle = (start+end)/2 = 1, end = 3
+                      [...]   0      1
+            line 62 sort(a, start, middle)
+                        [...]    0      1
+                    sort(int *a, start, end)
+                        Recursion 1(1)
+                            a = [90, 70, 20, 10, 40, 50, 20, 100]
+                            start = 0, middle = (start+end)/2 = 0, end = 1
+                                        [...] 0      0
+                            line 62 sort(a, start, middle) //finished
+                                Recursion 1(2) //finished 
+                                    [...]   0 + 1 = 1    1
+                            line 63 sort(a, middle + 1 , end)
+                                Recursion 1(2) //finished
+                                        [...] 0     0       1
+                            line 64 merge(a, start, middle, end);
+            line 63 sort(a, middle + 1, end); 
+
+    line 63 sort(a, middle + 1, end)
+
+
+*/
