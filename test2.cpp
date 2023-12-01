c8704ac53ee97bd437179f78628da46dd010a13b
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2023-12-01 13:30:17 -0500

    tut

---
+++ test2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+  private:
+    int NumberOfWheels = 20;
+    public:  
+    int NumberOfDoors = 10;
+
+
+    int getWheels(){
+        return NumberOfWheels;
+    }
+
+    int operator+(const Car &obj) const {
+        return NumberOfWheels + obj.NumberOfWheels;
+    }
+};
+
+
+int main(){
+
+    Car a1{};
+    Car a2{};
+
+
+    cout << a1.NumberOfDoors << endl;
+    cout << a1 + a2;
+
+    return 0;
+}
